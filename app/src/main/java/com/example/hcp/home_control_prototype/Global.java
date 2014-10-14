a1a24c8502554af3f7e063e99d6884ef4f53e451
ansonliang55 <ansonlia@cs.toronto.edu>
2014-10-14 12:30:42 -0400

    add global class, fix toast display and add gesture interface

---
+++ app/src/main/java/com/example/hcp/home_control_prototype/Global.java
@@ -0,0 +1,19 @@
+package com.example.hcp.home_control_prototype;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by ansonliang55 on 2014-10-14.
+ * This is a global glass that has function that is used globally (singleton software design pattern)
+ */
+public class Global {
+    public static void showToast(Context applicationContext, CharSequence text, int duration){
+        // Usage Global.showToast(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(applicationContext, text, duration);
+        toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
+    }
+}
