7460bc3cdfde12c232d539488afdc73925d6afac
Gary Graham <garygrantgraham@gmail.com>
2014-10-14 14:32:09 -0400

    WIP sparklib

--- app/src/main/java/com/example/hcp/home_control_prototype/AsyncAPITask.java
+++ app/src/main/java/com/example/hcp/home_control_prototype/AsyncAPITask.java
@@ -117,6 +117,58 @@ public abstract class AsyncAPITask extends AsyncTask<NameValuePair, Void, JSONAr
         }
         return null;
     }
+    private String parseEntity(HttpEntity entity){
+        String result = null;
+        try{
+            InputStream is = entity.getContent();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while((line = br.readLine())!= null){
+                sb.append(line).append("\n");
+            }
+            is.close();
+            result = sb.toString();
+            Log.i(TAG, "parseEntity() -> Received API Response -> " + result);
+
+        }
+        catch (Exception e){
+            Log.e(TAG, "parseEntity() -> Could not read API response! -> " + e.toString());
+
+        }
+        return result;
+    }
+
+    private JSONArray parseResultToJSON(String result){
+        JSONArray jArray;
+        try{
+
+            jArray = new JSONArray(result);
+            Log.i(TAG, "parseResultToJSON() -> " + "result was JSONArray, returning it. ");
+            return jArray;
+
+        }catch(JSONException e){
+
+            Log.i(TAG, "parseResultToJSON() -> " + "Result wasn't an array, creating an array from single object. ");
+            jArray = new JSONArray();
+
+            try {
+
+                JSONObject jObject = new JSONObject(result);
+                jArray.put(jObject);
+                return jArray;
+
+            } catch (JSONException e1) {
+
+                e1.printStackTrace();
+                Log.e(TAG, "Error parsing the json! -> " + e1.toString());
+
+            }
+
+            return null;
+        }
+    }
+
 
     @Override
     protected void onPostExecute(JSONArray jArray){
---
+++ app/src/main/java/com/example/hcp/home_control_prototype/Spark/LoginTask.java
@@ -0,0 +1,100 @@
+package com.example.hcp.home_control_prototype.Spark;
+
+import android.os.AsyncTask;
+import android.util.Base64;
+import android.util.Log;
+
+import com.example.hcp.home_control_prototype.Spark.Spark;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Created by garygraham on 2014-10-14.
+ */
+public class LoginTask extends AsyncTask<String, Void, ArrayList<Token>> {
+    private static final String URL = "https://api.spark.io/v1/access_tokens";
+    private static final String TAG = "LoginTask";
+    private Spark listener;
+
+    public LoginTask(Spark listener) {
+        this.listener = listener;
+    }
+
+
+        @Override
+        protected ArrayList<Token> doInBackground(String... credentials) {
+            String username = credentials[0];
+            String password = credentials[1];
+            ArrayList<Token> resultantTokens= new ArrayList<Token>();
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpUriRequest authRequest = new HttpGet(URL);
+
+            String header = username + ":" + password;
+            String base64EncodedCredentials = Base64.encodeToString(header.getBytes(), Base64.NO_WRAP);
+            authRequest.addHeader("Authorization", "Basic " + base64EncodedCredentials);
+
+            String result;
+            try {
+                HttpResponse response = httpclient.execute(authRequest);
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    HttpEntity entity = response.getEntity();
+
+                    //Static parsing class
+                    JSONArray jsonResult = ResponseParser.parseEntityToJSON(entity);
+
+                    for(int i = 0; i < jsonResult.length(); i++){
+
+                        try {
+                            JSONObject jObject = jsonResult.getJSONObject(i);
+                            String token_value = jObject.getString("token");
+                            String expiry_date = jObject.getString("expires_at");
+                            resultantTokens.add(new Token(token_value, expiry_date));
+                            Log.i(TAG, "login() -> " + "Found new token! -> " + token_value);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            return null;
+                        }
+                    }
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return resultantTokens;
+
+    }
+
+
+    @Override
+    public void onPostExecute(ArrayList<Token> result) {
+        for(Token token: result){
+            if(!this.listener.getTokens().contains(token)){
+                this.listener.addTokenToList(token);
+            }
+
+        }
+        if(this.listener.getTokens().size() > 0){
+            this.listener.rotateToken();
+        }
+        listener.findDevices();
+    }
+
+}
