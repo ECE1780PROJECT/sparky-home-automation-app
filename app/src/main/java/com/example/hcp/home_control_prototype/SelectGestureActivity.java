1dae05224b7fdce04cd822fb95c4ade48ee06cf2
ansonliang55 <ansonlia@cs.toronto.edu>
2014-10-16 20:57:50 -0400

    change image naming to lowercase

--- app/src/main/java/com/example/hcp/home_control_prototype/SelectGestureActivity.java
+++ app/src/main/java/com/example/hcp/home_control_prototype/SelectGestureActivity.java
@@ -1,21 +1,11 @@
 package com.example.hcp.home_control_prototype;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ListActivity;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.RemoteException;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.List;
 
 /**
  * Created by ansonliang55 on 2014-10-16.
@@ -29,9 +19,9 @@ public class SelectGestureActivity extends Activity {
             "BUMP UP",
     } ;
     Integer[] imageId = {
-            R.drawable.BumpR,
-            R.drawable.BumpL,
-            R.drawable.Handshake,
+            R.drawable.bumpr,
+            R.drawable.bumpl,
+            R.drawable.handshake,
     };
     ListView list;
     @Override
